259dc734f66dfa37dd41e3fdcea922e4c5e0e9eb
valerine <valerine.chemutai@student.moringaschool.com>
2023-08-09 16:51:58 +0300

    final

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,102 +1,105 @@
-//api token
-const access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJYUVRKTXFPRnpFTFg5ek5kMkpXS3l6TzhkTWVubTI1VFZnaGNjYkltZWY0RnNyMFMzNCIsImp0aSI6IjM5ZjU0ZDM1Y2E4ZjkyMDkwYWIxOTc1YjNmNTk3OTQ0M2ZhMzc2NDlkMjY4MmIxNTg1YmNmOTNjZWRiMTRkNDNjOGIxMzM2NzEyYWMzZTUzIiwiaWF0IjoxNjkxNTE0Njg5LCJuYmYiOjE2OTE1MTQ2ODksImV4cCI6MTY5MTUxODI4OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.BmXVqqUSDdEj7h0XswPQYI7SQ7LJ5dVgvE_JM3YM804FrQR_KuRUJknPgT66N_KgjRKMj1zWPVLRZs-ieI9mqQ2NzQiuZMJaTmVOcIw6ChUWF6TnVIcOsDh6sAZmjD2lOyp6wqX_S4gc9isEHPfLIjMjp5jqJ1FQC11I--OqGQsxF8qFXbd35PKo4gujGzrBie7mIhod5eZUZaCEzS3oIuX0Le7Haisg7SDkUW0fTVLERDQ6-vst_G-iMUZw-oQCwLvP4_R1yi8YWl7cfWLOjeeojwR_Co99avhhs8lzni0MM9T1jRBweVFMF2yjIKG1jKuZCeIUvysY1aQ62vf_eA";
+document.addEventListener('DOMContentLoaded', () => {
 
-//Initialize liked pets and like counts from local storage
-let likedPets = JSON.parse(localStorage.getItem('likedPets')) || {};
-let likeCounts = JSON.parse(localStorage.getItem('likeCounts')) || {};
-
-// Construct the request headers with the access token
-const headers = new Headers();
-headers.append('Authorization', `Bearer ${access_token}`);
-
-// Construct the query parameters
-const queryParams = new URLSearchParams();
-queryParams.append('type', 'dog'); 
-queryParams.append('breed', 'pug');
-queryParams.append('size', 'small');
-queryParams.append('gender', 'male');
-queryParams.append('age', 'baby'); 
-queryParams.append('limit', '15'); 
-
-//field expansion for photos and description
-queryParams.append('fields', 'photos,description');
-
-// Construct the full URL with query parameters
-const apiUrl = `https://api.petfinder.com/v2/animals?${queryParams.toString()}`;
-
-// Make the GET request to retrieve animals
-fetch(apiUrl, {
-  method: 'GET',
-  headers: headers
-})
-  .then(response => response.json())
-  .then(data => {
-    const petGallery = document.getElementById('petGallery');
-
-    // Process the data and populate the HTML
-    data.animals.forEach(animal => {
-      const petCard = document.createElement('div');
-      petCard.className = 'pet-card';
-      petCard.innerHTML = `
-        <img src="${animal.photos[0]?.medium}" alt="${animal.name}">
-        <h2>Name: ${animal.name}</h2>
-        <p>Type: ${animal.type}</p>
-        <p>Breed: ${animal.breeds.primary}</p>
-        <p>Age: ${animal.age}</p>
-        <p>Description: ${animal.description}</p>
-        <button class="like-button" data-pet-id="${animal.id}">Like</button>
-        <span class="likes-count">0</span>
-      `;
-    
-      const petGallery = document.getElementById('petGallery');
-      petGallery.appendChild(petCard);
-    
-
-      const likeButton = petCard.querySelector('.like-button');
-      const likesCountElement = petCard.querySelector('.likes-count');
-
-      likeButton.addEventListener('click', () => {
-        if (!likedPets[animal.id]) {
-          likedPets[animal.id] = true;
-          likeCounts[animal.id] = (likeCounts[animal.id] || 0) + 1;
-        } else {
-          likedPets[animal.id] = false;
-          likeCounts[animal.id] = (likeCounts[animal.id] || 1) - 1;
-        }
-
-        localStorage.setItem('likedPets', JSON.stringify(likedPets));
-        localStorage.setItem('likeCounts', JSON.stringify(likeCounts));
-
-        likeButton.classList.toggle('liked', likedPets[animal.id]);
-        likeButton.textContent = likedPets[animal.id] ? 'Liked' : 'Like';
-        likesCountElement.textContent = likeCounts[animal.id];
-      });
-      const observer = new MutationObserver(mutationsList => {
-        for (const mutation of mutationsList) {
-          if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-            // Liked state has changed
-            const likeButton = mutation.target;
-            const isLiked = likeButton.classList.contains('liked');
-            const petId = likeButton.dataset.petId;
-      
-            // Perform any actions you want when the liked state changes
-            console.log(`Liked state changed for pet ${petId}. Liked: ${isLiked}`);
-          }
-        }
-      });
-      
-      // Observe changes to the class attribute of all like buttons
-      const likeButtons = document.querySelectorAll('.like-button');
-      likeButtons.forEach(likeButton => {
-        observer.observe(likeButton, { attributes: true });
-      });
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching animals:', error);
-  });
-
-
-
-
-
+  //api credentials 
+   const client_id = "XQTJMqOFzELX9zNd2JWKyzO8dMenm25TVghccbImef4Fsr0S34";
+   const client_secret = "fDAs8C5BtaboLNtDbcgLocABWSCKV45zbcGE1tFd";
+ 
+   //create and configure the XMLHttpRequest object
+   const xhr = new XMLHttpRequest();
+   xhr.open('POST', "https://api.petfinder.com/v2/oauth2/token", true);
+   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+ 
+   //onload function
+   xhr.onload = function () { 
+     if (xhr.status === 200) {
+       const token_data = JSON.parse(xhr.responseText);
+       const access_token = token_data.access_token;
+ 
+       const headers = new Headers();
+       headers.append('Authorization', `Bearer ${access_token}`);
+ 
+       // Construct the query parameters
+       const queryParams = new URLSearchParams();
+       queryParams.append('type', 'dog'); 
+       queryParams.append('breed', 'retriever');
+       queryParams.append('size', 'small');
+       queryParams.append('gender', 'male');
+       queryParams.append('age', 'baby'); 
+       queryParams.append('limit', '15'); 
+ 
+       // Field expansion for photos and description
+       queryParams.append('fields', 'photos,description');
+ 
+       // Construct the full URL with query parameters
+       const apiUrl = `https://api.petfinder.com/v2/animals?${queryParams.toString()}`;
+ 
+       // Make the GET request to retrieve animals
+       fetch(apiUrl, {
+         method: 'GET',
+         headers: headers
+       })
+         .then(response => response.json())
+         .then(data => {
+           const petGallery = document.getElementById('petGallery');
+ 
+           // Process the data and populate the HTML
+           data.animals.forEach(animal => {
+             const petCard = document.createElement('div');
+             petCard.className = 'pet-card';
+             petCard.innerHTML = `
+               <img src="${animal.photos[0]?.medium}" alt="${animal.name}">
+               <h2>Name: ${animal.name}</h2>
+               <p>Type: ${animal.type}</p>
+               <p>Breed: ${animal.breeds.primary}</p>
+               <p>Age: ${animal.age}</p>
+               <p>Description: ${animal.description}</p>
+               <button class="like-button" data-pet-id="${animal.id}">Like</button>
+               <span class="likes-count">0</span>
+             `;
+           
+             petGallery.appendChild(petCard);
+ 
+             const likeButton = petCard.querySelector('.like-button');
+             const likesCountElement = petCard.querySelector('.likes-count');
+             const likedPets = JSON.parse(localStorage.getItem('likedPets')) || {};
+             const likeCounts = JSON.parse(localStorage.getItem('likeCounts')) || {};
+ 
+             likeButton.addEventListener('click', () => {
+               likedPets[animal.id] = !likedPets[animal.id];
+               likeCounts[animal.id] = (likeCounts[animal.id] || 0) + (likedPets[animal.id] ? 1 : -1);
+ 
+               localStorage.setItem('likedPets', JSON.stringify(likedPets));
+               localStorage.setItem('likeCounts', JSON.stringify(likeCounts));
+ 
+               likeButton.classList.toggle('liked', likedPets[animal.id]);
+               likeButton.textContent = likedPets[animal.id] ? 'Liked' : 'Like';
+               likesCountElement.textContent = likeCounts[animal.id];
+             });
+           });
+         })
+         .catch(error => {
+           console.error('Error fetching animals:', error);
+         });
+     } else {
+       console.error('Request failed. Status:', xhr.status);
+     }
+   };
+ 
+   // Construct the request data
+   const data = new URLSearchParams();
+   data.append('grant_type', 'client_credentials');
+   data.append('client_id', client_id);
+   data.append('client_secret', client_secret);
+ 
+   // Send the request
+   xhr.send(data);
+ 
+   // Add event listener to clearLikesButton
+   const clearLikesButton = document.getElementById('clearLikesButton');
+   clearLikesButton.addEventListener('click', () => {
+     localStorage.removeItem('likedPets');
+     localStorage.removeItem('likeCounts');
+   });
+ 
+ });
+ 
